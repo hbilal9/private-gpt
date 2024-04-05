0f710e7038bae1923f37294bcf5ac2d515be7153
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-04-05 21:09:03 +0500

    print msg updated

--- ingest.py
+++ ingest.py
@@ -154,7 +154,7 @@ def main():
     db.persist()
     db = None
 
-    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
+    print(f"Ingestion complete! You can now run gpt.py to query your documents")
 
 
 if __name__ == "__main__":
